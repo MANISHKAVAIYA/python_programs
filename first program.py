3f3ff046bcd4c84e768f18242c1eeb104106b88e
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-09 07:18:55 +0530

    python

---
+++ first program.py
@@ -0,0 +1,11 @@
+print("This is my python program")
+# condition apply
+a = 23
+if a < 50:
+    (
+        print("a is less than 50")
+    )
+else:
+    (
+        print("a is grater than 50")
+    )
